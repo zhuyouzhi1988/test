f5eaeeb6c58ca8a61482d6ebab24b8745aae346a
zhuyouzhi1988 <34804906+zhuyouzhi1988@users.noreply.github.com>
2024-03-08 17:48:09 +0800

    添加

---
+++ test.py
@@ -0,0 +1,15 @@
+#与运算
+print(True and True )    #==>True
+print(True and False)    #==>False
+print(False and True)    #==>False
+print(False and False)   #==>False
+
+#或运算
+print(True or True)      #==>True
+print(True or False)     #==>True
+print(False or True)     #==>True
+print(False or False)    #==>False
+
+#非运算
+print(not True)          #==>False
+print(not False)         #==>True
